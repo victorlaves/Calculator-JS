ecc7180fe3ae87fd7ea10c79086b96429b2aaadd
Victor Laves <victorpf2020@gmail.com>
2022-10-24 23:02:08 -0300

    Resultado

--- Calculadora.js
+++ Calculadora.js
@@ -1,8 +1,8 @@
-    var keep = "s"
-    var result = []
     var number1 = parseInt(prompt("Digite o primeiro Numero"))
     var number2 = parseInt(prompt("Digite o primeiro Numero"))
-
+    var keep = "s"
+    var result = []
+    var i = 0
 
 function Sum() {
     var sum = number1 + number2
@@ -20,15 +20,34 @@ function division(){
     var div = number1 / number2
     return div}
 
+function ExibirHitorico(){
+    console.log(result)
+}
 while(keep == "s")
 {
     var operation = prompt("Insira qual operação voce deseja... 1 = somar, 2 = subtrair, 3 = multiplicar, 4 = dividir")
-    if(operation == "1")
+    if(operation == "1"){
+    result[i] = Sum(number1, number2)
+    i++
     console.log(Sum(number1, number2))
-    else if(operation == "2")
+    }
+    else if(operation == "2"){
+    result[i] = Subtraction(number1, number2)
+    i++
     console.log(Subtraction(number1, number2))
-    else if(operation == "3")
+    }
+    else if(operation == "3"){
+    result[i] = multiplication(number1, number2)
+    i++
     console.log(multiplication(number1, number2))
+    }
+    else {
+        result[i] = division(number1, number2)
+        i++
+        console.log(division(number1, number2))
+    }
 
     keep = prompt("Deseja continuar as operações ?")
 }
+
+ExibirHitorico()
