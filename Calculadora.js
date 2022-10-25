ee88951c630e4b4a07c6cf4f7281f4762e29c192
Victor Laves <victorpf2020@gmail.com>
2022-10-24 22:52:46 -0300

    Logica da Operação

--- Calculadora.js
+++ Calculadora.js
@@ -1,11 +1,8 @@
-
-var keep = true
-
-while(keep)
-{
+    var keep = "s"
+    var result = []
     var number1 = parseInt(prompt("Digite o primeiro Numero"))
     var number2 = parseInt(prompt("Digite o primeiro Numero"))
-}
+
 
 function Sum() {
     var sum = number1 + number2
@@ -23,3 +20,15 @@ function division(){
     var div = number1 / number2
     return div}
 
+while(keep == "s")
+{
+    var operation = prompt("Insira qual operação voce deseja... 1 = somar, 2 = subtrair, 3 = multiplicar, 4 = dividir")
+    if(operation == "1")
+    console.log(Sum(number1, number2))
+    else if(operation == "2")
+    console.log(Subtraction(number1, number2))
+    else if(operation == "3")
+    console.log(multiplication(number1, number2))
+
+    keep = prompt("Deseja continuar as operações ?")
+}
