6eca6c8e5f74a6cf4c80237e522440afb44a7fbf
Victor Laves <victorpf2020@gmail.com>
2022-10-24 22:34:45 -0300

    Função soma

--- Calculadora.js
+++ Calculadora.js
@@ -1,5 +1,15 @@
 
+var keep = true
+
+while(keep)
+{
     var number1 = parseInt(prompt("Digite o primeiro Numero"))
     var number2 = parseInt(prompt("Digite o primeiro Numero"))
+}
+
+function Sum() {
+    var sum = number1 + number2
+    return sum}
+
 
 
